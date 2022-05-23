8c4111d3d92e3bc455236f9162b2b08188e4ebff
Samuel Santos <99606436+samuellsantos@users.noreply.github.com>
2022-05-23 20:00:55 -0300

    LandingPage BMW

---
+++ Projetos/BMW LandingPAge/assets/script.js
@@ -0,0 +1,12 @@
+function red(){
+    var red = document.getElementById('moto')
+    red.style.backgroundImage = "url(BMW1.png)";
+}
+function blue(){
+    var blue = document.getElementById('moto')
+    blue.style.backgroundImage = "url(BMW2.png)"
+}
+function black(){
+    var black = document.getElementById('moto')
+    black.style.backgroundImage = "url(BMW3.png)"
+}
