dfc5f823f64eec55125bf8878ff85ada6b6f7cad
Samuel Santos <samuka0102128@gmail.com>
2022-07-20 18:25:53 -0300

    Alterações na responsividade (ainda não concluído)

--- Projetos/Website AdventureTime/menu.js
+++ Projetos/Website AdventureTime/menu.js
@@ -3,13 +3,13 @@ let hideMenu = document.getElementById('hideMenu')
 let closeMenu = document.getElementById('close')
 
 menu.addEventListener('click', ()=>{
-    hideMenu.style.right = "50px"
+    hideMenu.style.top = "50px"
     menu.style.display = 'none'
     closeMenu.style.display = 'block'
 })
 
 function closeMENU(){
-    hideMenu.style.right = '-200px'
+    hideMenu.style.top = '-400px'
     menu.style.display = 'block'
     closeMenu.style.display = 'none'
 }
