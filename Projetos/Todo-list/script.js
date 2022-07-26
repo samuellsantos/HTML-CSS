2055ac7bdf0584a69dbc332b54bc3fa168c9265f
Samuel Santos <samuka0102128@gmail.com>
2022-07-25 21:59:42 -0300

    O usuário já pode colocar suas Tasks no programa!

---
+++ Projetos/Todo-list/script.js
@@ -0,0 +1,31 @@
+
+const text = document.getElementById('txt')
+const btn = document.getElementById('btn')
+
+
+
+
+btn.addEventListener('click', adicionarAlista)
+
+
+function adicionarAlista(){
+    if(text.value.length == 0){
+        alert('Você não adicionou nenhuma task')
+    }else{
+        const lista = document.getElementById('ulLista')
+    
+        
+        const paragrafo = document.createElement("li")
+        const texto = document.createTextNode(`- ${text.value}`);
+    
+    
+        paragrafo.appendChild(texto);
+        lista.appendChild(paragrafo);
+    
+        text.value = ""
+    }
+}
+
+
+
+
